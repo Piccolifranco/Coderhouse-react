79a6f67e8f48457a07d15b3be5e776dd4f9b973a
Piccolifranco <piccolifrancolucio@gmail.com>
2023-05-24 21:00:02 -0300

    Add photos

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -4,10 +4,10 @@ import { BsCartPlus } from 'react-icons/bs'
 export const Item = ({ item }) => {
   return (
     <div className='grid text-white bg-gray-600 rounded-md'>
-      <div className='h-60 w-full flex justify-center items-center bg-white p-6 rounded-t-md'>
-        <img className='h-60 rounded-md p-10' src={item.image} alt='Imagen del producto' />
+      <div className='h-60 w-full flex justify-center items-center bg-white rounded-t-md'>
+        <img className='h-60 rounded-md' src={item.image} alt='Imagen del producto' />
       </div>
-      <div>
+      <div className='flex flex-col gap-6'>
         <h2 className=' text-m font-bold pl-4'>{item.title}</h2>
         <p className=' text-sm font-bold p-4'>Price: ${item.price}</p>
         <p className='text-sm p-4'> Quantity: {item.quantity}</p>
