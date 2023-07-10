e7f1754a70bf55277d11cb591c9471509378a323
Piccolifranco <piccolifrancolucio@gmail.com>
2023-07-10 18:36:02 -0300

    cambios en item count

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -2,6 +2,7 @@ import { React, useContext, useState } from 'react'
 import { db } from '../../firebaseConfig'
 import { collection, addDoc } from 'firebase/firestore'
 import { carritoContext } from '../context/carritoContext'
+import { Link } from 'react-router-dom'
 
 export const CheckoutForm = () => {
   const { cart, clearCart, totalPrice } = useContext(carritoContext)
@@ -54,45 +55,55 @@ export const CheckoutForm = () => {
     setPhone('')
   }
 
-  return (
-    <div className='text-white'>
-      <h2 className='text-xl'>Checkout</h2>
-      <form onSubmit={handleForm} className='space-y-4'>
-        {cart.map(product => (
-          <div key={product.id} className=' rounded p-4'>
-            <p className='text-lg font-bold'>{product.title}</p>
-            <p>Quantity: {product.quantity}</p>
-            <p>Unit Price: ${product.price}</p>
+  console.log(cart)
+
+  if (cart.length > 0) {
+    return (
+      <div className='text-white'>
+        <h2 className='text-xl'>Checkout</h2>
+        <form onSubmit={handleForm} className='space-y-4'>
+          {cart.map(product => (
+            <div key={product.id} className=' rounded p-4'>
+              <p className='text-lg font-bold'>{product.title}</p>
+              <p>Quantity: {product.quantity}</p>
+              <p>Unit Price: ${product.price}</p>
+            </div>
+          ))}
+          <div>
+            <label htmlFor='name' className='font-semibold'>Name</label>
+            <input type='text' value={name} required onChange={(e) => (setName(e.target.value))} className='bg-white text-gray-800 px-2 py-1 rounded-md' />
           </div>
-        ))}
-        <div>
-          <label htmlFor='name' className='font-semibold'>Name</label>
-          <input type='text' value={name} required onChange={(e) => (setName(e.target.value))} className='bg-white text-gray-800 px-2 py-1 rounded-md' />
-        </div>
-        <div>
-          <label htmlFor='surname' className='font-semibold'>Surname</label>
-          <input type='text' value={surname} onChange={(e) => (setSurname(e.target.value))} className='bg-white text-gray-800 px-2 py-1 rounded-md' />
-        </div>
-        <div>
-          <label htmlFor='email' className='font-semibold'>E-mail</label>
-          <input type='text' value={email} onChange={(e) => (setEmail(e.target.value))} className='bg-white text-gray-800 px-2 py-1 rounded-md' />
-        </div>
-        <div>
-          <label htmlFor='emailConfirmation' className='font-semibold'>Repetir E-mail</label>
-          <input type='text' value={emailConfirmation} onChange={(e) => (setEmailConfirmation(e.target.value))} className='bg-white text-gray-800 px-2 py-1 rounded-md' />
-        </div>
-        <div>
-          <label htmlFor='phone' className='font-semibold'>Phone number</label>
-          <input type='text' value={phone} onChange={(e) => (setPhone(e.target.value))} className='bg-white text-gray-800 px-2 py-1 rounded-md' />
-        </div>
-        <div>
-          <h2 className='text-white'>Total: ${totalPrice}</h2>
-          <button type='submit' className='bg-blue-500 py-2 px-4 rounded text-white'>Enviar</button>
-        </div>
-      </form>
-      {orderID && (
-        <p>¡Gracias por tu compra! Vuelve pronto. Tu número de orden es {orderID}</p>
-      )}
-    </div>
-  )
+          <div>
+            <label htmlFor='surname' className='font-semibold'>Surname</label>
+            <input type='text' value={surname} onChange={(e) => (setSurname(e.target.value))} className='bg-white text-gray-800 px-2 py-1 rounded-md' />
+          </div>
+          <div>
+            <label htmlFor='email' className='font-semibold'>E-mail</label>
+            <input type='text' value={email} onChange={(e) => (setEmail(e.target.value))} className='bg-white text-gray-800 px-2 py-1 rounded-md' />
+          </div>
+          <div>
+            <label htmlFor='emailConfirmation' className='font-semibold'>Repetir E-mail</label>
+            <input type='text' value={emailConfirmation} onChange={(e) => (setEmailConfirmation(e.target.value))} className='bg-white text-gray-800 px-2 py-1 rounded-md' />
+          </div>
+          <div>
+            <label htmlFor='phone' className='font-semibold'>Phone number</label>
+            <input type='text' value={phone} onChange={(e) => (setPhone(e.target.value))} className='bg-white text-gray-800 px-2 py-1 rounded-md' />
+          </div>
+          <div>
+            <h2 className='text-white'>Total: ${totalPrice}</h2>
+            <button type='submit' className='bg-blue-500 py-2 px-4 rounded text-white'>Enviar</button>
+          </div>
+        </form>
+        {orderID && (
+          <p>¡Gracias por tu compra! Vuelve pronto. Tu número de orden es {orderID}</p>
+        )}
+      </div>
+    )
+  } else {
+    return (
+      <div className='text-white'>
+        <h3>No existen productos en el carrito, para comprar haga click <Link className=' underline cursor-pointer' to='/'>Aqui</Link></h3>
+      </div>
+    )
+  }
 }
